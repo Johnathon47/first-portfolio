8754cacb9d0895ba6cd36f1aa8d4f0621f7b231f
Johnathon47 <hei.johnathon.2@gmail.com>
2025-06-07 23:16:05 +0300

    feat:text white

--- components/Home/NavBar/Nav.tsx
+++ components/Home/NavBar/Nav.tsx
@@ -36,7 +36,7 @@ export default function Nav({ openNav }: Props) {
         } h-[12vh] z-[10] w-full transition-all duration-200`}>
             <div className="flex items-center h-full justify-between w-[95%] sm:w-[90%] xl:w-[80%] mx-auto">
                 {/* LOGO */}
-                <p className="bg-black">TOTOZAFY Johnathon</p>
+                <p className="bg-black text-white">TOTOZAFY Johnathon</p>
                 {/* Nav links */}
                 <div className="flex items-center space-x-10">
                     <div className="hidden lg:flex items-center space-x-8">
