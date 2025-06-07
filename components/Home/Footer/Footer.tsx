dea9910e6931287260095f45c061ce5d31e52224
Johnathon47 <hei.johnathon.2@gmail.com>
2025-06-07 23:40:51 +0300

    feat: change logo on my accronym name and change the name of corporate

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -1,15 +1,8 @@
-import Image from "next/image";
-
 export default function Footer() {
     return (
         <div className="pt-6 pb-6 bg-[#0f0715]">
             <div>
-            <Image 
-                src="/image/logo_hei.png" 
-                alt="LOGO HEI MY UNIVERSITY" 
-                width={80} 
-                height={80} 
-                className="mx-auto"/>
+                <p className="text-white text-2xl text-center">T.J.W</p>
             </div>
             <div className=" pt-10 flex items-center flex-wrap  justify-center space-x-10 text-white font-bold">
                 <div>Home</div>
@@ -17,7 +10,7 @@ export default function Footer() {
                 <div>Project</div>
                 <div>Contact</div>
             </div>
-            <p className="text-white/60 mt-6 text-center">© 2025 All Rights Reserved by Johnathon student of HEI</p>
+            <p className="text-white/60 mt-6 text-center">© 2025 All Rights Reserved by T.J.W</p>
         </div>
     );
 };
